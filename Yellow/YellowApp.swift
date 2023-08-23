da622d5078c5f743302945bf88a595f8d0144978
“dnzhelloo” <dnzhelloo@icloud.com>
2023-08-23 21:04:49 +0300

    Initial Commit

---
+++ Yellow/YellowApp.swift
@@ -0,0 +1,17 @@
+//
+//  YellowApp.swift
+//  Yellow
+//
+//  Created by Deniz on 23.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct YellowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
